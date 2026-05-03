6c251133bbff4bfceccc7a7d453e50800ed5d827
Lakirium <lakehaverlock@gmail.com>
2026-05-03 15:38:33 -0500

    Cat View (Mostly Done)
    All that needs to be added to complete this view is a way for the variables to carry over between different views, as well as the name to be set when account is created.

--- Task Cat/Views/CatView.swift
+++ Task Cat/Views/CatView.swift
@@ -1,13 +1,106 @@
 import SwiftUI
 
 struct CatView: View {
+    let moonGray = Color(white: 0.5, opacity: 0.7)
+    
+    @State private var catName = "Colonel Paws"
+    @State private var catImg = "cat_default"
+    
+    @State private var heart = 0
+    @State private var energy = 0
+
     var body: some View {
+        
         VStack {
-            Text("Cat")
-        }
+            Text("\(catName)")
+                .font(.title)
+                .bold()
+                .foregroundStyle(.purple)
+                .padding(.leading, 50)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+            
+            HStack{
+                Label("\(heart)", systemImage: "heart.circle")
+                    .foregroundStyle(.pink)
+                    .frame(width: 100, height: 40)
+                    .background(Capsule().fill(moonGray))
+                
+                Spacer()
+                
+                Label("\(energy)", systemImage: "bolt.fill")
+                    .foregroundStyle(.yellow)
+                    .frame(width: 100, height: 40)
+                    .background(Capsule().fill(moonGray))
+            }//HStack
+            .font(.title)
+            .padding(25)
+            
+            Image(catImg)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 250, height: 250)
+                .padding()
+            
+            HStack{
+                Button(action: {heart+=1; energy-=1; if heart >= 100 {heart = 100};
+                    catImg = "cat_eat";
+                    DispatchQueue.main.asyncAfter(deadline:.now() + 1.0){
+                        catImg = "cat_default"};
+                }){
+                    VStack{
+                        Text("Feed")
+                        Label("1", systemImage: "bolt.fill")
+                    }
+                }//Button
+                .disabled(energy < 1)
+
+                Button(action: {heart+=2; energy-=2; if heart >= 100 {heart = 100};
+                    catImg = "cat_pet";
+                    DispatchQueue.main.asyncAfter(deadline:.now() + 1.0){
+                        catImg = "cat_default"};
+                    }){
+                    VStack{
+                        Text("Pet")
+                        Label("2", systemImage: "bolt.fill")
+                    }
+                }//Button
+                .disabled(energy < 2)
+                
+                Button(action: {heart+=3; energy-=3; if heart >= 100 {heart = 100};
+                    catImg = "cat_treat";
+                    DispatchQueue.main.asyncAfter(deadline:.now() + 1.0){
+                        catImg = "cat_default"};
+                    }){
+                    VStack{
+                        Text("Treat")
+                        Label("3", systemImage: "bolt.fill")
+                    }
+                }//Button
+                .disabled(energy < 3)
+                
+                Button(action: {heart+=5; energy-=5; if heart >= 100 {heart = 100};
+                    catImg = "cat_play";
+                    DispatchQueue.main.asyncAfter(deadline:.now() + 1.0){
+                        catImg = "cat_default"};
+                    }){
+                    VStack{
+                        Text("Play")
+                        Label("5", systemImage: "bolt.fill")
+                    }
+                }//Button
+                .disabled(energy < 5)
+
+            }//HStack
+            .controlSize(.large)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 10))
+            .foregroundStyle(.yellow)
+            .tint(moonGray)
+
+        }//VStack
         .navigationBarBackButtonHidden(true)
-    }
-}
+    }//Body
+}//Struct
 
 #Preview {
     CatView()
